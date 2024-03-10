b280693cb899c7cd1c8d845421129ef225b83676
ravion223 <tablettaoson2016@gmail.com>
2024-03-10 18:53:52 +0200

    made model commentary, folder templates/blogs there I've created file base.html

--- blogs/models.py
+++ blogs/models.py
@@ -4,17 +4,33 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "posts")
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "posts")
 
-    name = models.CharField(max_length = 63)
+    title = models.CharField(max_length = 63)
     text = models.TextField()
     post_image = models.ImageField(null = True, blank = True, upload_to = "images/")
     posted_at = models.DateTimeField(auto_now_add = True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Post {self.name} by {self.user.username} - {self.posted_at}"
     
     class Meta:
         ordering = ["posted_at"]
         verbose_name = "Post"
         verbose_name_plural = "Posts"
+
+
+class Commentary(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "commentaries")
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name = "commentaries")
+
+    content = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) -> str:
+        return f"Commentart by {self.author.username} on post {self.post} - {self.posted_at}"
+    
+    class Meta:
+        ordering = ["posted_at"]
+        verbose_name = "Commentary"
+        verbose_name_plural = "Commentaries"
